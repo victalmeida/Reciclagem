b54aa3eb2462770999036359a2fc4ada6c099d0a
victor.dev <victor.dev@softmus.com.br>
2020-11-10 21:18:29 -0300

    Atualizando Rotas

--- app/rotas/rotas.js
+++ app/rotas/rotas.js
@@ -1,4 +1,5 @@
 import {app} from '../../index.js';
+
 app.get('/', function (req, res) {
     res.send('Hello World!');
   });
--- index.js
+++ index.js
@@ -1,6 +1,13 @@
+const { json } = require('express');
 const express = require('express')
 const app = express()
 const port = 5000
+
+
+const usuario = [];
+let AI = 0;
+
+app.use(json());
 
 app.get('/', (req, res) => {
   res.send('Novo Teste')
@@ -11,6 +18,42 @@ app.get('/teste', (req, res) => {
      console.log('Req:', req.query);
     res.send(resposta)
   })
+
+
+  // Rota dos Usuarios.
+
+  app.get('/usuario', (req, res) => {
+    
+    res.json(usuario);
+  });
+
+  app.post('/usuario', (req, res) => {
+    AI ++;
+    const {nome, email , senha } = req.body;
+    const newUser = {
+      id : AI,
+      nome,
+      email,
+      senha
+    }
+
+    usuario.push(newUser);
+
+    res.json(newUser);
+  });
+
+  app.put('/usuario/:id', (req, res) => {
+    
+    const {id} = req.params;
+    const {nome, email ,senha} = req.body;
+    const newUser = {id: parseInt(id) , nome, email, senha};
+    const index = usuario.findIndex( user => user.id == id );
+    usuario[index] = newUser;
+
+    res.send(newUser);
+  });
+
+ 
 
   
 
